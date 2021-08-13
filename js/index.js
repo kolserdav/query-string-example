b19da0bb7bdc0b9154e3e02d1cbee16d61be0b2d
kolserdav <serega12101983@gmail.com>
2021-08-13 15:04:02 +0700

    Turn settings

--- js/index.js
+++ js/index.js
@@ -33,9 +33,16 @@ function getValueFromQueryString(name) {
       null
     );
 }
+
+// Настройки вынесены отдельно
+const SITE = 'https://site-offer.ru';
+const PARAM_NAME = 'subid1';
+const TARGET_NAME = 'sub_id_1';
+const ELEMENT_SELECTOR = '#target-link';
+
 // Получаем ссылку на ссылку
-const link = document.querySelector('#target-link');
+const link = document.querySelector(ELEMENT_SELECTOR);
 // При помощи служебной функции парсим нужный параметр страницы
-const value = getValueFromQueryString('gclid');
+const value = getValueFromQueryString(PARAM_NAME);
 // Заменяем элементу ссылка атрибут href
-link.href = `https://site-offer.ru/?sub_id_1=${value}`;
+link.href = `${SITE}/?${TARGET_NAME}=${value}`;
